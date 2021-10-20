4b52bbb4e4d001770c2176dae666088f5de7bb91
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2021-10-20 17:04:44 -0500

    added switch for search, news, images, videos

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -23,7 +23,7 @@ export const Results = () => {
     case '/search':
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
-          {results?.results?.map(({ link, title }, index) => (
+          {results?.map(({ link, title }, index) => (
             <div key={index} className="md:w-2/5 w-full">
               <a href={link} target="_blank" rel="noreferrer">
                 <p className="text-sm ">{link.length > 30 ? link.substring(0, 40) : link}</p>
@@ -36,7 +36,7 @@ export const Results = () => {
     case '/images':
       return (
         <div className="flex flex-wrap justify-center items-center">
-          {results?.image_results?.map(({ image, link: { href, title } }, index) => (
+          {results?.map(({ image, link: { href, title } }, index) => (
             <a className="sm:p-3 p-5" href={href} key={index} target="_blank" rel="noreferrer">
               <img src={image?.src} alt={title} loading="lazy" />
               <p className="w-36 break-words text-sm mt-2">{title}</p>
@@ -45,9 +45,42 @@ export const Results = () => {
         </div>
       );
     case '/news':
-      return 'SEARCH';
+      return (
+        <div className="flex flex-wrap justify-between space-y-6 sm:px-56 items-center">
+          {results?.map(({ links, id, source, title }) => (
+            <div key={id} className="md:w-2/5 w-full">
+              <a
+                href={links?.[0].href}
+                target="_blank"
+                rel="noreferrer"
+                className="hover:underline"
+              >
+                <p className="text-lg dark:text-blue-300 text-blue-700">{title}</p>
+                <div className="flex gap-4">
+                  <a href={source?.href} target="_blank" rel="noreferrer">
+                    {source?.href}
+                  </a>
+                </div>
+              </a>
+            </div>
+          ))}
+        </div>
+      );
     case '/videos':
-      return 'SEARCH';
+      return (
+        <div className="flex flex-wrap">
+          {results.map((video, index) => (
+            <div key={index} className="p-2">
+              <ReactPlayer
+                url={video.additional_links?.[0].href}
+                controls
+                width="355px"
+                height="200px"
+              />
+            </div>
+          ))}
+        </div>
+      );
 
     default:
       return 'ERROR';
